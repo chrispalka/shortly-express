10577cb57fd68c9faf8528481608d339e95ed179
Chris Palka <cpalka87gmail.com>
2020-06-24 20:02:52 -0600

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,17 @@
 const parseCookies = (req, res, next) => {
+  // console.log('REQUEST BODY: ', req.headers.cookie.split(';'));
+  var cookieObj = {};
+  if (req.headers.cookie) {
+    var cookieArray = req.headers.cookie.split('; ');
+    cookieArray.forEach(cookie => {
+      var tempArr = cookie.split('=');
+      cookieObj[tempArr[0]] = tempArr[1];
+    });
+  }
+  // console.log('COOKIE OBJECT! ', cookieObj);
+  req.cookies = cookieObj;
+  next();
 };
 
 module.exports = parseCookies;
+
