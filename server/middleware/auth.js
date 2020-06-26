96f5cce5809b64c85ee2f31db24cd77c62de62e7
Chris Palka <cpalka87gmail.com>
2020-06-25 20:14:56 -0600

    Complete Createsession Middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,8 +2,6 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  // updating session table when user logs in?
-  // check if session has hash... crosscheck with DB
   var cookie = req.cookies.shortlyid;
   // NO COOKIE PRESENT ON REQUEST
   if (!cookie) {
@@ -22,36 +20,32 @@ module.exports.createSession = (req, res, next) => {
       .catch(err => {
         console.log('Error! ', err);
       });
+  } else {
+    // COOKIE PRESENT ON REQUEST
+    var hash = cookie;
+    // console.log('cookie---->', cookie);
+    // Check if incoming cookie matches hash in DB
+    models.Sessions.get({ hash })
+      .then(sessionData => {
+        if (sessionData) {
+          // If matching - set the session object to the return sessionData
+          req.session = sessionData;
+          // console.log('SESSION DATA --->', sessionData);
+          next();
+          // if sessionData doesn't exist
+        } else {
+          models.Sessions.create()
+            .then(results => {
+              return models.Sessions.get({ id: results.insertId });
+            })
+            .then(sessionData => {
+              req.session = sessionData;
+              res.cookie('shortlyid', sessionData.hash);
+              next();
+            });
+        }
+      });
   }
-  // COOKIE PRESENT ON REQUEST
-  var hash = cookie;
-  // Check if incoming cookie matches hash in DB
-  return models.Sessions.get({ hash })
-    .then(sessionData => {
-      if (sessionData) {
-        // If matching - set the session object to the return sessionData
-        req.session = sessionData;
-        // ** Things To Note: the below console log of sessionData has added user info added from tests **
-        console.log('SESSION DATA --->', sessionData);
-      }
-      // Below I was trying to clear the cookie sent from the request that was malicious (no match in DB found) and assign a new cookie to our response:
-
-      /******
-      res.clearCookie('shortlyid');
-      models.Sessions.create()
-        .then(results => {
-          return models.Sessions.get({ id: results.insertId });
-        })
-        .then(sessionData => {
-          res.cookie('shortlyid', sessionData.hash);
-        });
-        ******/
-      next();
-    })
-    .catch(err => {
-      console.log('Error! ', err);
-    });
-
 };
 
 /************************************************************/
